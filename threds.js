4c6dbfa010756a9d33ae2256fffcf5b506b6de90
shubham <shubham.badola@medibox.in>
2018-10-27 19:46:11 +0530

    Is node really single threaded

--- threds.js
+++ threds.js
@@ -2,5 +2,11 @@ const crypto = require('crypto');
 
 const start = Date.now();
 crypto.pbkdf2('a','b',100000,512,'sha512',() =>{
-  console.log('1:' , Date.now() - start); // returns 643 on local 
+  console.log('1:' , Date.now() - start); // returns 647 on local
   })
+  crypto.pbkdf2('a','b',100000,512,'sha512',() =>{
+    console.log('2:' , Date.now() - start); // returns 646 on local
+    })
+
+
+    //Is node really single threaded .......
