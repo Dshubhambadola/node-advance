c3d9a7c3a22166115729b29ba8542a9cfacd3ece
shubham <shubham.badola@medibox.in>
2018-10-31 01:58:29 +0530

    OS level task does not use threadpool no blocking

--- async.js
+++ async.js
@@ -2,9 +2,25 @@
 const https = require('https');
 const start = Date.now();
 
-https.request('https://www.google.com',res =>{
-  res.on('data',() =>{})
-  res.on('end',() =>{
-    console.log(Date.now() - start);
-  });
-}).end()
+
+function doRequest() {
+  https.request('https://www.google.com',res =>{
+    res.on('data',() =>{})
+    res.on('end',() =>{
+      console.log(Date.now() - start);
+    });
+  }).end()
+}
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+
+
+// So on an OS level task node libuv file take a help of OS async helpers to perform
+// the task in a parallel manner
+
+// As a result all the Task are done parallel and without using a threadpool
