c26a45e922638681ed258254feb622a186d815b4
shubham <shubham.badola@medibox.in>
2018-11-02 00:59:37 +0530

    Blocking the event loop

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();  // setting up an express server for node clustring
 
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {}
+}   // Blocking event loop for some time (To prove that node is not good in handling computation intensive tasks)
+
 app.get('/',(req,res) =>{
+  doWork(7000); 
   res.send('Hi there')
 });
 
