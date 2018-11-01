084dda208c90af85e9a0bb3914a964a83f92976e
shubham <shubham.badola@medibox.in>
2018-11-02 01:19:04 +0530

    Creating node cluster..

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const express = require('express');
+const cluster = require('cluster');
+// Is the file being executed in master mode?
+if (cluster.isMaster){
+  // Causes index.js to executed in *again* in child mode
+  cluster.fork();
+}else {
+  // Im a child  , Im act like a server
+  const express = require('express');
 const app = express();  // setting up an express server for node clustring
 
 function doWork(duration) {
@@ -7,8 +14,9 @@ function doWork(duration) {
 }   // Blocking event loop for some time (To prove that node is not good in handling computation intensive tasks)
 
 app.get('/',(req,res) =>{
-  doWork(7000); 
+  doWork(3000);
   res.send('Hi there')
 });
 
 app.listen(3000);
+};
