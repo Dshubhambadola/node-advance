6626a2f932f3611b754b42e3d3995016f589c10a
shubham <shubham.badola@medibox.in>
2018-11-02 21:17:52 +0530

    To know how node handle cluster and thread working together

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
+
+process.env.UV_THREADPOOL_SIZE = 1  // just to know how clustring work with single and multihread 
 const cluster = require('cluster');
 // Is the file being executed in master mode?
 if (cluster.isMaster){
   // Causes index.js to executed in *again* in child mode
   cluster.fork();
-  cluster.fork(); // To run node server in a cluster mode
+  //cluster.fork(); // To run node server in a cluster mode
 }else {
   // Im a child  , Im act like a server
   const express = require('express');
 const app = express();  // setting up an express server for node clustring
+const crypto = require('crypto');
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
-}   // Blocking event loop for some time (To prove that node is not good in handling computation intensive tasks)
 
 app.get('/',(req,res) =>{
-  doWork(5000);
-  res.send('Hi there')
+  crypto.pbkdf2('a','b',100000,512,'sha512',() =>{
+    res.send('Hi there')
+    })
+
 });
 
 app.get('/fast',(req,res) =>{
